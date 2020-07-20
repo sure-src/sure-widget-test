ef98510cc53f5c96e1f14a67b98ef5866c0540f7
Kirill <qz0010@mail.ru>
2020-07-20 16:02:15 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import SureWidget from 'sure-widget-x';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'sure-widget-test';
+  constructor() {
+    const w = new SureWidget('799f4725-af8d-4fc3-839b-853b50417df4', {openOnInit: true});
+  }
 }
